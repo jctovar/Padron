9376bbeba357b2c3df02de82df1bc1ad83a39b42
Juan Carlos Tovar Gomez <jctovar@comunidad.unam.mx>
2016-02-22 09:09:51 -0600

    20160222

--- Padron/MainWindow.xaml.vb
+++ Padron/MainWindow.xaml.vb
@@ -1,45 +1,74 @@
 ﻿Imports System.Net
 Imports System.IO
 Imports Newtonsoft.Json
+Imports System.Text.RegularExpressions
 Class MainWindow
     Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
-        Me.Search()
+        If validateUsername(txtUsername.Text) Then
+            Me.GetStudent(txtUsername.Text)
+        Else
+            MessageBox.Show("Ingresa un número de cuenta valido!")
+
+            txtUsername.Focus() 'Return Focus
+            txtUsername.Clear() 'Clear TextBox
+        End If
     End Sub
-    Private Sub Search()
-        Dim request As WebRequest = WebRequest.Create("https://galadriel.ired.unam.mx:3000/padron/068589242")
-        request.ContentType = "application/json; charset=utf-8"
-        Dim response As WebResponse = request.GetResponse()
-
-        Console.WriteLine(CType(response, HttpWebResponse).StatusDescription)
-        ' Get the stream containing content returned by the server.
-        Dim dataStream As Stream = response.GetResponseStream()
-        ' Open the stream using a StreamReader for easy access.
-        Dim reader As New StreamReader(dataStream)
-        ' Read the content.
-        Dim responseFromServer As String = reader.ReadToEnd()
-        ' Display the content.
-        Console.WriteLine(responseFromServer)
-
-
-        responseFromServer = "{""student"":" + responseFromServer + "}"
-
-
+    Private Sub GetStudent(username As String)
+        Dim request As HttpWebRequest
+        Dim response As HttpWebResponse = Nothing
+        Dim reader As StreamReader = Nothing
         Dim obj As Container
-        obj = JsonConvert.DeserializeObject(Of Container)(responseFromServer)
 
-        txtUsername.Text = obj.student(0).username.ToString
-        txtFirstname.Text = obj.student(0).firstname.ToString
-        txtLastname.Text = obj.student(0).lastname.ToString
-        txtEmail.Text = obj.student(0).email.ToString
-        txtField.Text = obj.student(0).career_name.ToString
-        txtGeneration.Text = obj.student(0).generation.ToString
-        'txtUsername.Text = obj.student(0).curp.ToString
+        Dim geturl As String = String.Format("https://galadriel.ired.unam.mx:3000/padron/{0}", username)
+
+        Try
+            ' Create the web request  
+            request = DirectCast(WebRequest.Create(geturl), HttpWebRequest)
+
+            ' Get response  
+            response = DirectCast(request.GetResponse(), HttpWebResponse)
+
+            ' Get the response stream into a reader  
+            reader = New StreamReader(response.GetResponseStream())
+
+            ' Console application output  
+            Dim jsontext As String = reader.ReadToEnd()
+            ' Display the content.
+            Console.WriteLine(jsontext)
+
+
+            jsontext = "{""student"":" + jsontext + "}"
 
 
 
-        ' Clean up the streams and the response.
-        reader.Close()
-        response.Close()
+            obj = JsonConvert.DeserializeObject(Of Container)(jsontext)
+
+
+            txtFirstname.Text = obj.student(0).firstname.ToString
+            txtLastname.Text = obj.student(0).lastname.ToString
+            txtEmail.Text = obj.student(0).email.ToString
+            txtField.Text = obj.student(0).career_name.ToString
+            txtGeneration.Text = obj.student(0).generation.ToString
+            txtPhone.Text = obj.student(0).phone.ToString
+            txtPostalCode.Text = obj.student(0).postal_code.ToString
+            txtPassword.Text = obj.student(0).password.ToString
+            txtCurp.Text = obj.student(0).curp.ToString
+            txtHeadquarters.Text = obj.student(0).headquarters.ToString
+
+
+            ' Clean up the streams and the response.
+
+        Catch ex As Exception
+            ' Show the exception's message.
+            MessageBox.Show(ex.Message)
+            'MessageBox.Show("No se encontro un registro valido, verifique!", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information) 'Inform User
+        Finally
+            If Not response Is Nothing Then response.Close()
+            txtUsername.Focus() 'Return Focus
+            reader.Close()
+            response.Close()
+        End Try
+
 
     End Sub
     Public Class Container
@@ -58,4 +87,11 @@ Class MainWindow
         Public headquarters As String
         Public career_name As String
     End Class
+    Private Function validateUsername(varUsername As String) As Boolean
+        If Not Regex.Match(varUsername, "^[0-9]{9}$", RegexOptions.IgnoreCase).Success Then 'Only Letters
+            Return False 'Boolean = False
+        Else
+            Return True 'Everything Fine
+        End If
+    End Function
 End Class
