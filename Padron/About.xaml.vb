496e82e2e3eb77ee82a032b428da49a6aa51c55a
JUAN CARLOS TOVAR GOMEZ <jctovar@comunidad.unam.mx>
2016-03-04 18:34:52 -0600

    20160304

--- Padron/About.xaml.vb
+++ Padron/About.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Deployment
+﻿Imports System.Deployment.Application
 Public Class About
     Public Sub New()
 
@@ -10,5 +10,13 @@ Public Class About
 
         'Información de Copyright
         Copyright.Text = My.Application.Info.Copyright
+
+        ' Version.Text = My.Application.
+
+        If System.Diagnostics.Debugger.IsAttached = False Then
+            Me.Version.Text = String.Format("Versión {0}", Deployment.Application.ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString)
+        Else
+            Me.Version.Text = String.Format("Versión {0}", "Debug mode")
+        End If
     End Sub
 End Class
